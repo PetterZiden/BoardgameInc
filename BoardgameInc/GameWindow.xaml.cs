b02a2355d525f8abc82a202017edffc4ef90a729
petzi854 <petzi854@student.liu.se>
2017-11-18 15:34:50 +0100

    Grunden för UI

---
+++ BoardgameInc/GameWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BoardgameInc.Logic_layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace BoardgameInc
+
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            LogicController LC = new LogicController();
+            LC.GameLoop();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Title = e.GetPosition(this).ToString();
+        }
+    }
+}
