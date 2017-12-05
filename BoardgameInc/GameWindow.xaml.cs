d1c98665388ab77a43d0edb7e8ecf8dd530f6edb
oskal070 <oskal070@PCPUL3-004>
2017-12-05 16:05:14 +0100

    Saker ändrade

--- BoardgameInc/GameWindow.xaml.cs
+++ BoardgameInc/GameWindow.xaml.cs
@@ -40,6 +40,8 @@ namespace BoardgameInc
             Button current = (Button)sender;
             int row = Grid.GetRow(current);
             int col = Grid.GetColumn(current);
+
+            controller.nextShotInput(Convert.ToInt32("" + row + col));
             
         }
 
@@ -50,16 +52,14 @@ namespace BoardgameInc
             int row = Grid.GetRow(current);
             int col = Grid.GetColumn(current);
 
-            current.Background = Brushes.Aquamarine;
-          
-            
-            
+            //current.Background = Brushes.Aquamarine;
+
         }
 
         private void btn_MouseLeave(object sender, MouseEventArgs e)
         {
             Button current = (Button)sender;
-            current.Background = Brushes.Gray;
+            //current.Background = Brushes.Gray;
           
         }
 
@@ -68,35 +68,43 @@ namespace BoardgameInc
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
                     Button temp = (Button)GameGrid.Children.OfType<Button>().Where(x => Grid.GetRow(x) == i && Grid.GetColumn(x) == j).FirstOrDefault();
+                    var brush = new ImageBrush();
                     int tempInt = Convert.ToInt32("" + i + j);
+                    Console.WriteLine(grid[tempInt]);
                     if (grid[tempInt] == 0)
                     {
                         Console.WriteLine("GAMEWINDOW - " + i + j);
-                        BitmapImage bmp = new BitmapImage();
+                        /*BitmapImage bmp = new BitmapImage();
                         Uri u = new Uri("Images/Water.jpg", UriKind.RelativeOrAbsolute);
                         bmp.UriSource = u;
                         Image tempImages = new Image();
                         tempImages.Source = bmp;
-                        temp.Content = tempImages;
+                        temp.Content = tempImages; */
+                        brush.ImageSource = new BitmapImage(new Uri("Images/Water.jpg", UriKind.Relative));
+                        temp.Background = brush;
 
                     }
                     else if (grid[tempInt] == 1)
                     {
-                        BitmapImage bmp = new BitmapImage();
+                        /*BitmapImage bmp = new BitmapImage();
                         Uri u = new Uri("Images/Miss.jpg", UriKind.RelativeOrAbsolute);
                         bmp.UriSource = u;
                         Image tempImages = new Image();
                         tempImages.Source = bmp;
-                        temp.Content = tempImages;
+                        temp.Content = tempImages; */
+                        brush.ImageSource = new BitmapImage(new Uri("Images/Miss.jpg", UriKind.Relative));
+                        temp.Background = brush;
                     }
                     else if (grid[tempInt] == 2)
                     {
-                        BitmapImage bmp = new BitmapImage();
+                        /*BitmapImage bmp = new BitmapImage();
                         Uri u = new Uri("Images/Hit.jpg", UriKind.RelativeOrAbsolute);
                         bmp.UriSource = u;
                         Image tempImages = new Image();
                         tempImages.Source = bmp;
-                        temp.Content = tempImages;
+                        temp.Content = tempImages;*/
+                        brush.ImageSource = new BitmapImage(new Uri("Images/Hit.jpg", UriKind.Relative));
+                        temp.Background = brush;
                     }
                     else
                     {
